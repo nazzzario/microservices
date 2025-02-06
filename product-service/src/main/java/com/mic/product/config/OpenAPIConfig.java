dd508e8dc8545c490a1b0966965aaec17ba6a5a8
nazzzario <nazzzario23@gmail.com>
2025-02-06 10:43:43 +0200

    add Swagger to Product Service

--- product-service/src/main/java/com/mic/product/config/OpenAPIConfig.java
+++ product-service/src/main/java/com/mic/product/config/OpenAPIConfig.java
@@ -4,6 +4,7 @@ package com.mic.product.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -14,20 +15,19 @@ import java.util.Collections;
 @Configuration
 public class OpenAPIConfig {
 
-    @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("client-service")
-                .pathsToMatch("/api/**")
-                .build();
-    }
 
     @Bean
-    public OpenAPI springShopOpenAPI() {
+    public OpenAPI productServiceOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Client APIs")
-                        .version("v1.0.0"))
+                        .title("Product Service API")
+                        .description("This is the REST API for Product Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0"))
+
+                        .contact(new Contact()
+                                .email("test@email.com")
+                                .name("Nazar Krasnovoronka")))
                 .servers(Collections.singletonList(new Server().url("http://localhost:8080")));
     }
 }
