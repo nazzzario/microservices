96286786821dcd6b5db530210565aab29b25d26d
nazzzario <nazzzario23@gmail.com>
2025-02-06 12:57:13 +0200

    add swagger to inventory service

---
+++ inventory-service/src/main/java/com/mic/inventory/config/OpenAPIConfig.java
@@ -0,0 +1,31 @@
+package com.mic.inventory.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
+
+@Configuration
+public class OpenAPIConfig {
+
+
+    @Bean
+    public OpenAPI orderServiceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Order Service API")
+                        .description("This is the REST API for Order Service")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0"))
+                        .contact(new Contact()
+                                .email("test@email.com")
+                                .name("Nazar Krasnovoronka")))
+                .servers(Collections.singletonList(new Server().url("http://localhost:8082")));
+    }
+}
